94ab6f6ff0fb9b96ee0edf156255b4e523a78f5b
bhatbhav <bhavya.bhat@unisys.com>
2024-11-04 10:08:56 -0500

    css updates

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -4,24 +4,17 @@ import './AboutUs.css';
 function AboutUs() {
   return (
     <div className="about-us-container">
-      {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <h1 className="about-us-heading">About Us</h1>
+      <p className="about-us-description">Welcome to Nisarga Paradise Nursery, where green meets serenity!</p>
       <p className="about-us-content">
         Welcome to our nursery, where we believe every plant tells a story! Our carefully curated selection features a diverse range of healthy, vibrant plants, perfect for both novice gardeners and seasoned horticulturists. Whether you're looking to brighten your home with lush houseplants, create a stunning garden landscape, or find the perfect gift for a fellow plant lover, we have something for everyone. Each plant is nurtured with care, ensuring you receive top-quality specimens ready to thrive in your space. Explore our collection and let us help you grow your green paradise!
       </p>
-      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-      <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
-      </p>
-      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-
+      <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p>
       <p className="about-us-content">
         Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
         beauty of nature right at your doorstep.
       </p>
+      <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p>
     </div>
   );
 }
