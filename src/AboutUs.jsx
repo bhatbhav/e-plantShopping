8ee2259fb3e63afc5d274850dfeb244e61d86c4a
bhatbhav <bhavya.bhat@unisys.com>
2024-11-05 01:28:52 -0500

    about us info update

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,16 +5,17 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       <h1 className="about-us-heading">About Us</h1>
-      <p className="about-us-description">Welcome to Nisarga Paradise Nursery, where green meets serenity!</p>
       <p className="about-us-content">
-        Welcome to our nursery, where we believe every plant tells a story! Our carefully curated selection features a diverse range of healthy, vibrant plants, perfect for both novice gardeners and seasoned horticulturists. Whether you're looking to brighten your home with lush houseplants, create a stunning garden landscape, or find the perfect gift for a fellow plant lover, we have something for everyone. Each plant is nurtured with care, ensuring you receive top-quality specimens ready to thrive in your space. Explore our collection and let us help you grow your green paradise!
+        Welcome to Nisarga Nursery, where we believe every plant tells a story! Our carefully curated selection features a diverse range of healthy, vibrant plants, perfect for both novice gardeners and seasoned horticulturists. Whether you're looking to brighten your home with lush houseplants, create a stunning garden landscape, or find the perfect gift for a fellow plant lover, we have something for everyone. 
+      </p>
+      <p className="about-us-content">
+        Each plant is nurtured with care, ensuring you receive top-quality specimens ready to thrive in your space. Explore our collection and let us help you grow your green paradise!
       </p>
       <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p>
       <p className="about-us-content">
         Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
         beauty of nature right at your doorstep.
       </p>
-      <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p>
     </div>
   );
 }
