d737cc9da290b99631b034f608e6f7f030d92827
bhatbhav <bhavya.bhat@unisys.com>
2024-11-07 18:54:18 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -270,7 +270,7 @@ const handlePlantsClick = (e) => {
         <div>
              <div className="navbar" style={styleObj}>
             <div className="tag">
-               <div className="luxury">
+               <div className="luxury" style={{cursor:"pointer"}} onClick={props.toLanding}>
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                <a href="/" style={{textDecoration:'none'}}>
                         <div>
