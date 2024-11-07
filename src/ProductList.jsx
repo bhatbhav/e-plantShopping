ba72b3a51c43152e7818bf3425f43be5c395a6ab
bhatbhav <bhavya.bhat@unisys.com>
2024-11-07 08:42:54 -0500

    props added in product.js file

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 
-function ProductList() {
+function ProductList(props) {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
